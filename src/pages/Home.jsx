812945db43c8cfab3a30b6e86a6859331aa5bcc4
Shreyas <shreyasbandekar01@gmail.com>
2025-08-13 20:09:34 +0530

    Add Upload component to Home page for image upload functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../components/Header'
 import Process from '../components/Process'
 import BgSlider from '../components/BgSlider'
+import Upload from '../components/Upload'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Header />
       <Process />
       <BgSlider />
+      <Upload />
     </div>
   )
 }
