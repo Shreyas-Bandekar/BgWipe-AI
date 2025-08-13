8ced2475b1e6d07ce68baf6b9da17185f69d4d1a
Shreyas <shreyasbandekar01@gmail.com>
2025-08-13 11:55:50 +0530

    Add Process component to Home page for background removal steps

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/Header'
+import Process from '../components/Process'
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Process />
     </div>
   )
 }
