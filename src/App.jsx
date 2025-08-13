6ba6ea5ecac3475bec35b49c40c2ddb11a81c9ab
Shreyas <shreyasbandekar01@gmail.com>
2025-08-13 20:36:16 +0530

    Add Footer component to enhance page layout with branding and social links

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,18 @@ import Home from './pages/Home'
 import Result from './pages/Result'
 import BuyCredit from './pages/BuyCredit'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="min-h-screen bg-slate-100">
       <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/result' element={<Result />} />
-        <Route path='/buy' element={<BuyCredit />} />
-      </Routes>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/result' element={<Result />} />
+          <Route path='/buy' element={<BuyCredit />} />
+        </Routes>
+      <Footer />
     </div>
   )
 }
