480a10aaa1fcdb5d7e3fbef51cf337c27361c72d
Shreyas <shreyasbandekar01@gmail.com>
2025-08-13 19:57:36 +0530

    Add BgSlider component to Home page for interactive background removal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import Process from '../components/Process'
+import BgSlider from '../components/BgSlider'
 
 const Home = () => {
   return (
     <div>
       <Header />
       <Process />
+      <BgSlider />
     </div>
   )
 }
