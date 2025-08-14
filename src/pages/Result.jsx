c0e842eb1f0b2846bef0c957aab8b733a304df7e
Shreyas <shreyasbandekar01@gmail.com>
2025-08-14 11:24:38 +0530

    Refactor Result component for improved code readability and consistency

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import {assets} from '../assets/assets.js'  
+<div className='flex flex-col'></div>
+import { assets } from '../assets/assets.js'
 
 const Result = () => {
   return (
@@ -8,33 +8,40 @@ const Result = () => {
         <div className='flex flex-col sm:grid grid-cols-2 gap-8'>
 
           {/* Original Image */}
-            <div>
-              <p className='font-semibold text-gray-600 mb2'>Original</p>
-              <img className='rounded-md border' src={assets.image_w_bg} alt="" />
-            </div>
+          <div>
+            <p className='font-semibold text-gray-600 mb-2'>Original</p>
+            <img className='rounded-md border' src={assets.image_w_bg} alt="" />
+          </div>
 
           {/* Edited Image */}
           <div className='flex flex-col'>
-            <p className='font-semibold text-gray-600 mb2'>Background Removed</p>
-
+            <p className='font-semibold text-gray-600 mb-2'>Background Removed</p>
             <div className='rounded-md border border-gray-300 h-full relative bg-layer overflow-hidden'>
               {/* <img className='rounded-md border' src={assets.image_wo_bg} alt="" /> */}
               {/* Loader */}
               <div className='absolute right-1/2 bottom-1/2 transform translate-x-1/2 translate-y-1/2'>
-                <div className='border-4 border-fuchsia-600 rounded-full h-12 w-12 border-t-transparent animate-spin'>
-
-                </div>
+                <div className='border-4 border-fuchsia-600 rounded-full h-12 w-12 border-t-transparent animate-spin'></div>
               </div>
             </div>
           </div>
-          
-          {/* Buttons */}
-          <div className='flex justify-center sm:justify-end items-center flex-wrap '>
-            <button>Try another image</button>
-            <a href="">Download Image</a>
-          </div>
+
         </div>
 
+        {/* Buttons */}
+        <div className='flex flex-col sm:flex-row justify-center sm:justify-end items-center gap-4 mt-8 flex-wrap'>
+          <button
+            className="px-8 py-2.5 text-sm text-fuchsia-600 border border-fuchsia-500 rounded-full hover:scale-105 transition-all duration-700"
+          >
+            Try another image
+          </button>
+          <a
+            href=""
+            className="px-8 py-2.5 text-white text-sm bg-gradient-to-r from-blue-600 to to-fuchsia-500 rounded-full hover:scale-105 transition-all duration-700"
+            download
+          >
+            Download Image
+          </a>
+        </div>
       </div>
     </div>
   )
