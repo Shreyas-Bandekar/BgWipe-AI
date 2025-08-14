e69912abeb4b7df012ec150b2b06afde9f856571
Shreyas <shreyasbandekar01@gmail.com>
2025-08-14 10:23:20 +0530

    Add Result component to display original and background-removed images

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import {assets} from '../assets/assets.js'  
 
 const Result = () => {
   return (
-    <div>Result</div>
+    <div className='mx-4 my-3 lg:mx-44 mt-14 min-h-[75vh]'>
+      <div className='bg-white rounded-lg px-8 py-6 drop-shadow-sm'>
+        <div className='flex flex-col sm:grid grid-cols-2 gap-8'>
+
+          {/* Original Image */}
+            <div>
+              <p className='font-semibold text-gray-600 mb2'>Original</p>
+              <img className='rounded-md border' src={assets.image_w_bg} alt="" />
+            </div>
+
+          {/* Edited Image */}
+          <div className='flex flex-col'>
+            <p className='font-semibold text-gray-600 mb2'>Background Removed</p>
+
+            <div className='rounded-md border border-gray-300 h-full relative bg-layer overflow-hidden'>
+              {/* <img className='rounded-md border' src={assets.image_wo_bg} alt="" /> */}
+              {/* Loader */}
+              <div className='absolute right-1/2 bottom-1/2 transform translate-x-1/2 translate-y-1/2'>
+                <div>
+
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
