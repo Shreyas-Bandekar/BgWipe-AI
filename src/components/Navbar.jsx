2e279d59323542d34a1e88d80dab79b2c6b9cd3d
Shreyas <shreyasbandekar01@gmail.com>
2025-08-15 10:20:52 +0530

    Refactor Navbar component to improve code readability and structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,28 @@
 import React from 'react'
-import {assets} from '../assets/assets.js'
-import {Link} from 'react-router-dom'
-import { useClerk } from '@clerk/clerk-react'
+import { assets } from '../assets/assets.js'
+import { Link } from 'react-router-dom'
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
 
 const Navbar = () => {
 
     const { openSignIn } = useClerk()
+    const { isSignedIn } = useUser()
 
-return (
-    <div className='flex justify-between items-center mx-4 py-3 lg:mx-44'>
-        <Link to='/'><img src={assets.logo} alt="logo" className='w-32 sm:w-44'/></Link>
-        <button onClick={() => openSignIn({})} className='bg-zinc-800 text-white flex item-center gap-1 py-2 px-4 sm:px-8 sm:py-3 text-sm rounded-full'>
-            Get Started <img src={assets.arrow_icon} alt="arrow" />
-        </button>
-    </div>
-)
+    return (
+        <div className='flex justify-between items-center mx-4 py-3 lg:mx-44'>
+            <Link to='/'><img src={assets.logo} alt="logo" className='w-32 sm:w-44' /></Link>
+            {
+                isSignedIn ? 
+                <div>
+                    <UserButton />
+                </div>
+                :<button onClick={() => openSignIn({})} className='bg-zinc-800 text-white flex item-center gap-1 py-2 px-4 sm:px-8 sm:py-3 text-sm rounded-full'>
+                    Get Started <img src={assets.arrow_icon} alt="arrow" />
+                </button>
+            }
+
+        </div>
+    )
 }
 
 export default Navbar
