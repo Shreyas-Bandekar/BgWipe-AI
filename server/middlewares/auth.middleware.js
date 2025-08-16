3485b544b85d6029c3457b82d1f63d0caec6c141
Shreyas <shreyasbandekar01@gmail.com>
2025-08-16 16:47:45 +0530

    Refactor authUser middleware to improve token handling and error responses

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -1,11 +1,17 @@
+import { messageInRaw } from "svix";
 import jwt from jsonwebtoken
 
-const authUser= async (req, res, next) => {
+const authUser = async (req, res, next) => {
     try {
-        const token = req.headers.authorization.split(" ")[1];
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
+        const token = req.headers
+        if (!token) {
+            return res.status(401).json({ success: false, message: "Unauthorized login again" });
+        }
+
+        const token_decode = jwt.decode(token);
+        req.body.clerkId = token_decode.clerkId;
         next();
+
     } catch (error) {
         console.log(error.message);
         res.status(401).json({ success: false, error: "Unauthorized" });
