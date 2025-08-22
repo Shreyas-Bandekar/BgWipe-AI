f1b6572aaa41be4c82aa12e0912be094bcda442b
Shreyas <shreyasbandekar01@gmail.com>
2025-08-22 10:24:45 +0530

    Implement MongoDB connection in db.js and set up server in server.js

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import connectDB from './config/db.js';
+
+const PORT = process.env.PORT || 4000;
+const app = express();
+await connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('API is running...'));
+
+app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`)});
+
+export default app;
